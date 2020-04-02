701bf206bddb4798ff3928e247cfd2c52c867844
Denner Marchi <denner883@hotmail.com>
2020-04-02 15:49:13 -0300

    Arrumando bug de rota no app.use

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin', adminRouter);
-app.use('/usuarios', usersRouter);
+app.use('/usuarios', usuariosRouter);
 
 // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
