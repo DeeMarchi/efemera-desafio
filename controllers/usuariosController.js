7d2326ece7a3ef564f17654fd70e3a37f3e6e5c3
Denner Marchi <denner883@hotmail.com>
2020-04-02 16:33:41 -0300

    reenvidando controller e definando post

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -5,6 +5,23 @@ const fs = require('fs');
 
 let usuarioJSONPath = path.join('db', 'usuarios.json');
 
+const lerInfo = (nomePasta, nomeArquivo) => {
+    let dadosArquivo = [];
+    const codificacao = 'utf-8';
+  
+    let pathArquivo = path.join(nomePasta, nomeArquivo);
+  
+    if (fs.existsSync(pathArquivo)) {
+      dadosArquivo = fs.readFileSync(pathArquivo, { encoding: codificacao });
+      dadosArquivo = JSON.parse(dadosArquivo);
+    } else {
+      console.log(`[Atenção] arquivo ${pathArquivo} não foi encontrado`);
+    }
+  
+    return dadosArquivo;
+};
+
+
 const usuariosController = {
    registroForm: (req, res) => {
        res.render('registroUsuarios', {title: "Registro"});
@@ -12,6 +29,8 @@ const usuariosController = {
 
    salvarForm: (req, res) => {
        let { nome, email, senha} = req.body;
+
+        let infoUsuario = { nome, email, senha };
 
         
 
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -5,6 +5,7 @@ const usuariosController = require('../controllers/usuariosController');
 /* GET users listing. */
 
 router.get('/criar', usuariosController.registroForm);
+router.post('/criar', usuariosController.salvarForm);
 router.get('/login', usuariosController.loginForm);
 
 module.exports = router;
